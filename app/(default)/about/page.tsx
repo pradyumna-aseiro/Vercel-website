f0ba6e6df03d04c082b03105789c1b4f3e9ca418
Prad <pradyumna@aseiro.in>
2025-04-04 14:34:06 +0530

    changed card icon size

--- app/(default)/about/page.tsx
+++ app/(default)/about/page.tsx
@@ -41,22 +41,22 @@ export default function AboutPage() {
         <div>
           <h2 className="text-3xl font-bold mb-10">What Sets Us Apart</h2>
           <div className="grid md:grid-cols-2 gap-8">
-          <div className="bg-white rounded-xl p-6 text-center shadow-md hover:shadow-lg transition">
+          <div className="bg-slate-50 p-6 rounded-xl shadow-md hover:shadow-lg transition">
               <FaPuzzlePiece className="text-blue-600 text-3xl mb-3" />
               <h3 className="text-xl font-semibold mb-2">Custom-Tailored Systems</h3>
               <p className="text-gray-600">We create modular solutions that fit perfectly into your factory setup—whether it's a small upgrade or a full transformation.</p>
             </div>
-            <div className="bg-white rounded-xl p-6 text-center shadow-md hover:shadow-lg transition">
+            <div className="bg-slate-50 p-6 rounded-xl shadow-md hover:shadow-lg transition">
               <FaTools className="text-blue-600 text-3xl mb-3" />
               <h3 className="text-xl font-semibold mb-2">End-to-End Support</h3>
               <p className="text-gray-600">From planning and integration to training and maintenance—we stay with you every step of the way.</p>
             </div>
-            <div className="bg-white rounded-xl p-6 text-center shadow-md hover:shadow-lg transition">
+            <div className="bg-slate-50 p-6 rounded-xl shadow-md hover:shadow-lg transition">
               <FaRobot className="text-blue-600 text-3xl mb-3" />
               <h3 className="text-xl font-semibold mb-2">Innovative Integration</h3>
               <p className="text-gray-600">We blend machine vision, robotics, and real-time controls for faster inspection and lower costs—up to 70% inspection time reduction.</p>
             </div>
-            <div className="bg-white rounded-xl p-6 text-center shadow-md hover:shadow-lg transition">
+            <div className="bg-slate-50 p-6 rounded-xl shadow-md hover:shadow-lg transition">
               <FaCheckCircle className="text-blue-600 text-3xl mb-3" />
               <h3 className="text-xl font-semibold mb-2">Proven Track Record</h3>
               <p className="text-gray-600">Over 10,000 clients trust our systems to deliver reliable performance across industries.</p>
