1baed3b4ba20ca614b20fd917b8019b0815d5735
Prad <pradyumna@aseiro.in>
2025-04-15 16:55:55 +0530

    smooth transition between pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 // app/layout.tsx
 import "./css/style.css";
 import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
+import { ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,20 +19,24 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
         className={`${inter.variable} bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
       >
-        <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
-        <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
-          {children}
-        </div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={Math.random()} // Force animation on route change
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip"
+          >
+            {children}
+          </motion.div>
+        </AnimatePresence>
       </body>
     </html>
   );
